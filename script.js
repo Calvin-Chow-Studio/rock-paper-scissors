0e75d04e1f453e616809e4f3e863e1d73f86321d
Calvin-Chow-Studio <chowjm.calvin@gmail.com>
2022-03-10 12:12:46 +0700

    Game is working as intended, missing cosmetics and loops

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 console.log("hello");
 
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
 function computerPlay() {
-    const choice = ["rock", "paper", "scissors"]
+    const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random()*choice.length)];
 }
 console.log(computerPlay());
+
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "draw";
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+                 playerSelection === "paper" && computerSelection === "rock" || 
+                 playerSelection === "scissors" && computerSelection === "scissors") { 
+                    return "win";
+    } else {
+        return "lose";
+    }
+}
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection));
 
 // function compResult() {
 //     if (computerPlay() === 1) {
@@ -17,34 +36,28 @@ console.log(computerPlay());
 // }
 // console.log(compResult());
 
-
-function playRound(playerSelection,computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "win";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "lose";
-    } else if (playerSelection == "rock" && computerSelection == "rock") {
-        return "draw";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "win";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "lose";
-    } else if (playerSelection == "paper" && computerSelection == "paper") {
-        return "draw";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "lose";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "win";
-    } else { 
-        return "draw";
-    }
-}
-
+// function playRound(playerSelection,computerSelection) {
+//     if (playerSelection = computerSelection) {
+//         return "win";
+//     // } else if (playerSelection == "rock" && computerSelection == "paper") {
+//     //     return "lose";
+//     // } else if (playerSelection == "rock" && computerSelection == "rock") {
+//     //     return "draw";
+//     // } else if (playerSelection == "paper" && computerSelection == "rock") {
+//     //     return "win";
+//     // } else if (playerSelection == "paper" && computerSelection == "scissors") {
+//     //     return "lose";
+//     // } else if (playerSelection == "paper" && computerSelection == "paper") {
+//     //     return "draw";
+//     // } else if (playerSelection == "scissors" && computerSelection == "rock") {
+//     //     return "lose";
+//     // } else if (playerSelection == "scissors" && computerSelection == "paper") {
+//     //     return "win";
+//     } else { 
+//         return "draw";
+//     }
+// }
 
 
-
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound());
 
 // rock paper scissors
